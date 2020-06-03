a8a60a92cb144213a97b26dbe95209fb0cd32f74
ygnaiih1680 <ygnaiih1680@mju.ac.kr>
2020-06-03 15:13:42 +0900

    spring sync

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import RootContainer from "./views/root/RootContainer";
+import Login from "./views/Home/Login";
 
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
 
@@ -10,7 +11,7 @@ const App = () =>{
       <BrowserRouter>
         <React.Suspense fallback={loading()}>
           <Switch>
-            {/*<Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />*/}
+            <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
             {/*<Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />*/}
             {/*<Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />*/}
             {/*<Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />*/}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import axios from 'axios';
 
 const Home = (props) => {
+    const [state, setState] = useState({});
+    const callback = (data) => {
+        console.log(data)
+        // setState({id: data.id});
+    }
+
+    const ax = () => {
+        axios.post('/test', {name: "sj99@pheonix.com", password: 'psj'}, {baseURL: 'http://localhost:8080', withCredentials: true,}).then(r => callback(r.data))
+    }
+
     return(
-        <div>Home</div>
+        <div>Home {state.id} <input type="button" onClick={ax}/></div>
     )
 }
 
