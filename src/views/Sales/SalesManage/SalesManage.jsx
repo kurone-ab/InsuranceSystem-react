e2fcde67d0904bcb1f66b977fc200462a5f64a5f
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-06-16 22:41:51 +0900

    [SalesManage] Add table

--- src/views/Sales/SalesManage/SalesManage.jsx
+++ src/views/Sales/SalesManage/SalesManage.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {lazy} from "react";
+import CustomizableTable from "../../global/CustomizableTable";
+import {connect} from 'react-redux'
+import {useGetAxios} from "../../global/useAxios";
+import {loadSalesInstructionList} from "../../../globalStore";
+import Loading from "../../global/Loading";
 
-const SalesManage = () => {
+const header = {
+    id: '번호',
+    title: '이름',
+    employeeNum: '사원번호',
+    workingDistrict: '관할 구역',
+}
+
+const SalesManage = ({loadSalesInstructionList}) => {
+    useGetAxios({url: 'instruction/employee/Employee'})
+
+    const renderData = []
+    
+
     return(
-        <div>SalesManage</div>
+        <CustomizableTable tableRowData={renderData} tableTitle = '영업 관리' tableHeader = {header}>
+
+        </CustomizableTable>
     )
 }
 
