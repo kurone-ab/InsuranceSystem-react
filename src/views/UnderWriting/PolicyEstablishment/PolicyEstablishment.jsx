8ff296e4270666bfe14fd769a85e2f8895ba5757
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-06-17 17:57:06 +0900

    Add Tables to [PolicyEstablishment]

--- src/views/UnderWriting/PolicyEstablishment/PolicyEstablishment.jsx
+++ src/views/UnderWriting/PolicyEstablishment/PolicyEstablishment.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import CustomizableTable from "../../global/CustomizableTable";
+import {connect} from 'react-redux';
+import {useGetAxios} from "../../global/useAxios";
+import Loading from "../../global/Loading";
+
+const header = {
+    id: '번호',
+    title: '보험 종류',
+    employeeNum: '적/부 판단율',
+    workingDistrict: 'U/W 요청 건수',
+}
 
 const PolicyEstablishment = () => {
+    const renderData = []
+
     return(
-        <div>PolicyEstablishment</div>
+        <CustomizableTable tableRowData={renderData} tableTitle = '인수 지침 관리' tableHeader = {header}>
+
+        </CustomizableTable>
     )
 }
 
